84e0802d24b44b6951ca71d177899dd95fb32403
Jhon Arias <jhonarmont@hotmail.com>
2018-08-03 20:59:15 -0500

    Se añaden todas las vistas y archivos necesarios para la creacion de base de datos

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem 'sqlite3'
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem "rake"
--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 # En la app sinatra crear un formulario que reciba: 
 #Nombre, apellido, edad, email, dirección, genero y redirigirlo a una vista donde muestre toda esta 
 #información de forma ordenada, utilicen bootstrap en todos los elementos, agregando un header en el layout.
-require 'sinatra'
+require 'bundler/setup'
+Bundler.require(:default)
+require 'sqlite3'
+require './models.rb'
 
+set :database, "sqlite3:myblogdb.sqlite3"
 
 
 get '/login' do
@@ -15,16 +19,33 @@ post '/login' do
     redirect '/'
 end
 
-get "/form" do
-    erb :form
+get '/' do
+    @posts = Post.all
+    erb :home
 end
 
-post "/form-return" do
-    @name=params[:name]
-    @surname=params[:surname]
-    @age=params[:age]
-    @email=params[:email]
-    @address=params[:address]
-    @gender=params[:gender]
+post '/post' do
+    @post = Post.create(title: params[:title], body: params[:body])
+    redirect '/'
 end
+
+get '/post/:id' do
+    @post = Post.find(params[:id])
+    erb :post_page
+end
+
+
+
+# get "/form" do
+#     erb :form
+# end
+
+# post "/form-return" do
+#     @name=params[:name]
+#     @surname=params[:surname]
+#     @age=params[:age]
+#     @email=params[:email]
+#     @address=params[:address]
+#     @gender=params[:gender]
+# end
 
